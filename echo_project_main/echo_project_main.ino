d717114d877fa18fca47e8154fc2ab8cc20cf3d1
Vinh Nguyen <vinhsnguyen86@gmail.com>
2024-01-13 01:09:49 -0800

    continuing to correct the error of my ways

--- echo_project_main/echo_project_main.ino
+++ echo_project_main/echo_project_main.ino
@@ -30,279 +30,279 @@ void BPM(int bpmUpdate) {
 }
 
 void a() {
-  dit()
-  dah()
-  next_letter()
+  dit();
+  dah();
+  next_letter();
 }
 
 void b() {
-  dah()
-  dit()
-  dit()
-  dit()
-  next_letter()
+  dah();
+  dit();
+  dit();
+  dit();
+  next_letter();
 }
 
 void c(){
-  dah()
-  dit()
-  dah()
-  dit()
-  next_letter()
+  dah();
+  dit();
+  dah();
+  dit();
+  next_letter();
 }
 
 void d() {
-  dah()
-  dit()
-  dit()
-  next_letter()
+  dah();
+  dit();
+  dit();
+  next_letter();
 }
 
 void e() {
-  dit()
-  next_letter()
+  dit();
+  next_letter();
 }
 
 void f(){
-  dit()
-  dit()
-  dah()
-  dit()
-  next_letter()
+  dit();
+  dit();
+  dah();
+  dit();
+  next_letter();
 }
 
 void g() {
-  dah()
-  dah()
-  dit()
-  next_letter()
+  dah();
+  dah();
+  dit();
+  next_letter();
 }
 
 void h() {
-  dit()
-  dit()
-  dit()
-  dit()
-  next_letter()
+  dit();
+  dit();
+  dit();
+  dit();
+  next_letter();
 }
 
 void i(){
-  dit()
-  dit()
-  next_letter()
+  dit();
+  dit();
+  next_letter();
 }
 
 void j() {
-  dit()
-  dah()
-  dah()
-  dah()
-  next_letter()
+  dit();
+  dah();
+  dah();
+  dah();
+  next_letter();
 }
 
 void k() {
-  dah()
-  dit()
-  dah()
-  next_letter()
+  dah();
+  dit();
+  dah();
+  next_letter();
 }
 
 void l(){
-  dit()
-  dah()
-  dit()
-  dit()
-  next_letter()
+  dit();
+  dah();
+  dit();
+  dit();
+  next_letter();
 }
 
 void m() {
-  dah()
-  dah()
-  next_letter()
+  dah();
+  dah();
+  next_letter();
 }
 
 void n() {
-  dah()
-  dit()
-  next_letter()
+  dah();
+  dit();
+  next_letter();
 }
 
 void o(){
-  dah()
-  dah()
-  dah()
-  next_letter()
+  dah();
+  dah();
+  dah();
+  next_letter();
 }
 
 void p() {
-  dit()
-  dah()
-  dah()
-  dit()
-  next_letter()
+  dit();
+  dah();
+  dah();
+  dit();
+  next_letter();
 }
 
 void q() {
-  dah()
-  dah()
-  dit()
-  dah()
-  next_letter()
+  dah();
+  dah();
+  dit();
+  dah();
+  next_letter();
 }
 
 void r(){
-  dit()
-  dah()
-  dit()
-  next_letter()
+  dit();
+  dah();
+  dit();
+  next_letter();
 }
 
 void s() {
-  dit()
-  dit()
-  dit()
-  next_letter()
+  dit();
+  dit();
+  dit();
+  next_letter();
 }
 
 void t() {
-  dah()
-  next_letter()
+  dah();
+  next_letter();
 }
 
 void u(){
-  dit()
-  dit()
-  dah()
-  next_letter()
+  dit();
+  dit();
+  dah();
+  next_letter();
 }
 
 void v() {
-  dit()
-  dit()
-  dit()
-  dah()
-  next_letter()
+  dit();
+  dit();
+  dit();
+  dah();
+  next_letter();
 }
 
 void w() {
-  dit()
-  dah()
-  dah()
-  next_letter()
+  dit();
+  dah();
+  dah();
+  next_letter();
 }
 
 void x(){
-  dah()
-  dit()
-  dit()
-  dah()
-  next_letter()
+  dah();
+  dit();
+  dit();
+  dah();
+  next_letter();
 }
 
 void y() {
-  dah()
-  dit()
-  dah()
-  dah()
-  next_letter()
+  dah();
+  dit();
+  dah();
+  dah();
+  next_letter();
 }
 
 void z() {
-  dah()
-  dah()
-  dit()
-  dit()
-  next_letter()
+  dah();
+  dah();
+  dit();
+  dit();
+  next_letter();
 }
 
 void num0(){
-  dah()
-  dah()
-  dah()
-  dah()
-  dah()
-  next_letter()
+  dah();
+  dah();
+  dah();
+  dah();
+  dah();
+  next_letter();
 }
 
 void num1() {
-  dit()
-  dah()
-  dah()
-  dah()
-  dah()
-  next_letter()
+  dit();
+  dah();
+  dah();
+  dah();
+  dah();
+  next_letter();
 }
 
 void num2() {
-  dit()
-  dit()
-  dah()
-  dah()
-  dah()
-  next_letter()
+  dit();
+  dit();
+  dah();
+  dah();
+  dah();
+  next_letter();
 }
 
 void num3(){
-  dit()
-  dit()
-  dit()
-  dah()
-  dah()
-  next_letter()
+  dit();
+  dit();
+  dit();
+  dah();
+  dah();
+  next_letter();
 }
 
 void num4() {
-  dit()
-  dit()
-  dit()
-  dit()
-  dah()
-  next_letter()
+  dit();
+  dit();
+  dit();
+  dit();
+  dah();
+  next_letter();
 }
 
 void num5(){
-  dit()
-  dit()
-  dit()
-  dit()
-  dit()
-  next_letter()
+  dit();
+  dit();
+  dit();
+  dit();
+  dit();
+  next_letter();
 }
 
 void num6() {
-  dah()
-  dit()
-  dit()
-  dit()
-  dit()
-  next_letter()
+  dah();
+  dit();
+  dit();
+  dit();
+  dit();
+  next_letter();
 }
 
 void num7(){
-  dah()
-  dah()
-  dit()
-  dit()
-  dit()
-  next_letter()
+  dah();
+  dah();
+  dit();
+  dit();
+  dit();
+  next_letter();
 }
 
 void num8() {
-  dah()
-  dah()
-  dah()
-  dit()
-  dit()
-  next_letter()
+  dah();
+  dah();
+  dah();
+  dit();
+  dit();
+  next_letter();
 }
 
 void num9(){
-  dah()
-  dah()
-  dah()
-  dah()
-  dit()
-  next_letter()
+  dah();
+  dah();
+  dah();
+  dah();
+  dit();
+  next_letter();
 }
 
 void dah() {
