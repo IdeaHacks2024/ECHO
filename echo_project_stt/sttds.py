2cb547a6546e0385eb12837937965a51a407576d
HersheysCB <harshinshah29@gmail.com>
2024-01-14 01:53:26 -0800

    fix bugs on speech to text

--- echo_project_stt/sttds.py
+++ echo_project_stt/sttds.py
@@ -1,6 +1,8 @@
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import json
+import requests
 
 model_path = '/Users/harshinshah/Downloads/vosk-model-en-us-0.22'  # !Replace with your model path!
 if not os.path.exists(model_path):
@@ -21,9 +23,12 @@ print("Speak now, press Ctrl+C to stop.")
 
 
 while True:
-    data = stream.read(4096)
+    data = stream.read(4096, exception_on_overflow = False)
     if recognizer.AcceptWaveform(data):
-        print(recognizer.Result())
+        
+        data_to_send = json.loads(recognizer.Result())['text']
+        print(data_to_send)
+        requests.get(f'http://10.0.0.1/{data_to_send}_MORSE')
     # else:
     #     print(recognizer.PartialResult())
 
