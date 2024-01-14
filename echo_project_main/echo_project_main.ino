9e31a565f8420295e7c4434cc5936e7319a8ffc3
philipdoucla <philipminhdo@ucla.edu>
2024-01-13 17:35:18 -0800

    Implement interface to change modes

--- echo_project_main/echo_project_main.ino
+++ echo_project_main/echo_project_main.ino
@@ -1,17 +1,177 @@
-const int motorPin = 1;
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <Servo.h>
+
+/////// please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "handler";        // your network SSID (name)
+char pass[] = "password";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+WiFiServer server(80);
+
+// int base_pin = 2;
+// int arm_pin = 3;
+int led =  LED_BUILTIN;
+int status = WL_IDLE_STATUS;
+
+// int prev_base = 150;
+
+// int ARM_DELAY = 15;
+// int ARM_DEL = 1;
+// int BASE_DELAY = 10;
+
+const int motorPin = 3;
 
 const int unit = 500;
 int bpm = 120;
+
+int state = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(motorPin, OUTPUT);
   Serial.begin(9600);
   Serial.println("Enter BPM:");
+
+  Serial.println("Access Point Web Server");
+  // pinMode(led, OUTPUT);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // by default the local IP address of will be 192.168.4.1
+  // you can override it with the following:
+  WiFi.config(IPAddress(10, 0, 0, 1));
+
+  // print the network name (SSID);
+  Serial.print("Creating access point named: ");
+  Serial.println(ssid);
+
+  // Create open network. Change this line if you want to create an WEP network:
+  status = WiFi.beginAP(ssid, pass);
+
+  if (status != WL_AP_LISTENING) {
+    Serial.println("Creating access point failed");
+    // don't continue
+    while (true);
+  }
+
+  // wait 10 seconds for connection:
+  delay(10000);
+
+  // start the web server on port 80
+  server.begin();
+
+  // you're connected now, so print out the status
+  printWiFiStatus();
+
+  // base.attach(base_pin);
+  // arm.attach(arm_pin);
+
+  // base.write(150);
+  // arm.write(55);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // compare the previous status to the current status
+  if (status != WiFi.status()) {
+
+    // it has changed update the variable
+    status = WiFi.status();
+
+    if (status == WL_AP_CONNECTED) {
+      // a device has connected to the AP
+      Serial.println("Device connected to AP");
+    } else {
+      // a device has disconnected from the AP, and we are back in listening mode
+      Serial.println("Device disconnected from AP");
+    }
+
+  }
+
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/BUZZ\">here</a> Buzz Mode<br>");
+            client.print("Click <a href=\"/METRONOME\">here</a> Metronome Mode<br>");
+            client.print("Click <a href=\"/MORSE\">here</a> Morse Mode<br>");
+            client.print("Click <a href=\"/STOP\">here</a> Stop Mode<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+
+            // break out of the while loop:
+            break;
+          }
+
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+        
+        // raise arm
+        if (currentLine.endsWith("GET /BUZZ")) {
+            state = 1;
+        }
+
+        // lower arm
+        if (currentLine.endsWith("GET /MORSE")) {
+            state = 2;
+
+        }
+
+        if (currentLine.endsWith("GET /METRONOME")) {
+          // Serial.print(currentLine);
+
+          // String deg_string = currentLine.substring(currentLine.length()-10, currentLine.length()-7);
+          // Serial.print("BPM: ");
+          // Serial.println(deg_string);
+          // int deg = deg_string.toInt();
+          state = 3;
+        }
+
+        if (currentLine.endsWith("GET /STOP")) {
+          state = -1;
+        }
+      }
+    }
+  }
+  Serial.println(state);
+
   if (Serial.available() > 0) {
     int serial_bpm = Serial.parseInt();
     if (serial_bpm > 0) {
@@ -20,8 +180,31 @@ void loop() {
       Serial.println(bpm);
     }
   }
-  Serial.println(bpm);
-  BPM(bpm);
+
+  if (state == 3) {
+    Serial.println(bpm);
+    BPM(bpm);
+  }
+  if (state == -1) {
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 }
 
 void BPM(int bpmUpdate) {
