Fixed Bass Detector input overflow

echo_project_feel_the_music/bassDetector.py
@@ -2,15 +2,19 @@ import pyaudio
 import numpy as np
 from scipy.fft import rfft, rfftfreq
 import requests
+import time
+from pyfirmata import Arduino
 
 # Constants
 CHUNK = 16384  # Number of audio samples per frame
 FORMAT = pyaudio.paInt16  # Audio format (16-bit PCM)
 CHANNELS = 1  # Single channel for microphone
 RATE = 44100  # Sampling rate
-threshold = 90000
+threshold = 100000
 
-
+# port = '/dev/cu.usbmodem14401'
+# pin=3
+# board = Arduino(port)
 
 def analyze_audio(data, y):
    
@@ -22,20 +26,23 @@ def analyze_audio(data, y):
     xf = rfftfreq(CHUNK, 1 / RATE)
 
     # Analyze the frequency components
-    if np.any(np.abs(yf[(xf >= 20) & (xf <= 80)]) > threshold):
+    if np.any(np.abs(yf[(xf >= 10) & (xf <= 60)]) > threshold):
         print(y)
-        temp = requests.get(f'http://10.0.0.1/BUZZ')
+        requests.get(f'http://10.0.0.1/BUZZ')
+        # board.digital[pin].write(1)
+        # time.sleep(0.075)
+        # board.digital[pin].write(0)
 
 def main():
     p = pyaudio.PyAudio()
 
     # Open stream
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=0)
     y = 0
 
     try:
         while True:
-            data = stream.read(CHUNK)
+            data = stream.read(CHUNK, exception_on_overflow = False)
             analyze_audio(data, y)
             y += 1
     except KeyboardInterrupt: